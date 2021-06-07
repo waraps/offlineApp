d456d5ccad635217535c8ad2cb2f433a230eba1c
Manuel Meneses <manuelmenesesg@hotmail.com>
2021-06-07 14:06:36 -0400

    testing react-native-background-actions

--- App.js
+++ App.js
@@ -1,111 +1,114 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
+  ScrollView,
   View,
+  Text,
+  StatusBar,
+  TouchableOpacity,
 } from 'react-native';
+import {Header, Colors} from 'react-native/Libraries/NewAppScreen';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import BackgroundJob from 'react-native-background-actions';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+const sleep = time => new Promise(resolve => setTimeout(() => resolve(), time));
+
+const taskRandom = async taskData => {
+  await new Promise(async resolve => {
+    // For loop with a delay
+    const {delay} = taskData;
+    for (let i = 0; BackgroundJob.isRunning(); i++) {
+      console.log('Runned -> ', i);
+      await BackgroundJob.updateNotification({taskDesc: 'Runned -> ' + i});
+      await sleep(delay);
+    }
+  });
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+const options = {
+  taskName: 'Example',
+  taskTitle: 'Tarea',
+  taskDesc: 'ExampleTask desc',
+  taskIcon: {
+    name: 'ic_launcher',
+    type: 'mipmap',
+  },
+  progressBar: {
+    max: 100,
+    value: 0,
+    indeterminate: true,
+  },
+  color: 'aliceblue',
+  linkingURI: 'https://google.com',
+  parameters: {
+    delay: 1000,
+  },
+};
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+const App = () => {
+  let playing = BackgroundJob.isRunning();
+
+  /**
+   * Toggles the background task
+   */
+  const toggleBackground = async () => {
+    playing = !playing;
+    if (playing) {
+      try {
+        console.log('Trying to start background service');
+        await BackgroundJob.start(taskRandom, options);
+        console.log('Successful start!');
+      } catch (e) {
+        console.log('Error', e);
+      }
+    } else {
+      console.log('Stop background service');
+      await BackgroundJob.stop();
+    }
   };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <Header />
+          {global.HermesInternal == null ? null : (
+            <View style={styles.engine}>
+              <Text style={styles.footer}>Engine: Hermes</Text>
+            </View>
+          )}
+          <View style={styles.body}>
+            <TouchableOpacity
+              style={{height: 100, width: 100, backgroundColor: 'red'}}
+              onPress={toggleBackground}></TouchableOpacity>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  scrollView: {
+    backgroundColor: Colors.lighter,
   },
-  sectionTitle: {
-    fontSize: 24,
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
     fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
   },
 });
 
